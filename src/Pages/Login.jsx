44d69f81c6349b4165d9637bca3e58b5b53e1371
safimiazi <mohibullamiazi@gmail.com>
2023-12-21 19:02:08 +0600

    start authentication

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -14,51 +14,60 @@ import { Link } from "react-router-dom";
 
 
 const Login = () => {
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value
+    }
     return (
         <div className=" bg-blue-200 pt-10 md:pt-0 md:h-[665px] grid grid-cols-1 md:grid-cols-2 items-center justify-between md:gap-4 p-4">
-        <div className="md:ml-40">
-            <Card className="md:w-96">
-                <CardHeader
-                    variant="gradient"
-                    color="gray"
-                    className="mb-4 grid h-28 place-items-center"
-                >
-                    <Typography variant="h3" color="white">
-                        Sign in
-                    </Typography>
-                </CardHeader>
-                <CardBody className="flex flex-col gap-4">
-                    <Input label="Email" size="lg" />
-                    <Input label="Password" size="lg" />
-                    <div className="-ml-2.5">
-                        <Checkbox label="Remember Me" />
-                    </div>
-                </CardBody>
-                <CardFooter className="pt-0">
-                    <Button variant="gradient" fullWidth>
-                        Sign in
-                    </Button>
-                    <Typography variant="small" className="mt-6 flex justify-center">
-                        Don&apos;t have an account?
-                       <Link to="/register">
-                       <Typography
-                            as="a"
-                            href=""
-                            variant="small"
-                            color="blue-gray"
-                            className="ml-1 font-bold"
+            <div className="md:ml-40">
+                <form onSubmit={handleLogin}>
+                    <Card className="md:w-96">
+                        <CardHeader
+                            variant="gradient"
+                            color="gray"
+                            className="mb-4 grid h-28 place-items-center"
                         >
-                            register
-                        </Typography>
-                       </Link>
-                    </Typography>
-                </CardFooter>
-            </Card>
+                            <Typography variant="h3" color="white">
+                                Sign in
+                            </Typography>
+                        </CardHeader>
+                        <CardBody className="flex flex-col gap-4">
+                            <Input label="Email" size="lg" />
+                            <Input label="Password" size="lg" />
+                            <div className="-ml-2.5">
+                                <Checkbox label="Remember Me" />
+                            </div>
+                        </CardBody>
+                        <CardFooter className="pt-0">
+                            <Button type="submit" variant="gradient" fullWidth>
+                                Sign in
+                            </Button>
+                            <Typography variant="small" className="mt-6 flex justify-center">
+                                Don&apos;t have an account?
+                                <Link to="/register">
+                                    <Typography
+                                        as="a"
+                                        href=""
+                                        variant="small"
+                                        color="blue-gray"
+                                        className="ml-1 font-bold"
+                                    >
+                                        register
+                                    </Typography>
+                                </Link>
+                            </Typography>
+                        </CardFooter>
+                    </Card>
+                </form>
+            </div>
+            <div>
+                <Lottie className="h-[500px]" animationData={loginAnimation} />
+            </div>
         </div>
-        <div>
-            <Lottie className="h-[500px]" animationData={loginAnimation}/>
-        </div>
-    </div>
     );
 };
 
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -13,49 +13,60 @@ import Lottie from "lottie-react";
 import { Link } from "react-router-dom";
 
 const Register = () => {
+    const handleRegister = e => {
+     e.preventDefault()
+     const form = e.target;
+     const name = form.name.value;
+     const email = form.email.value;
+     const password = form.password.value
+     
+    }
     return (
         <div className=" bg-blue-200 pt-10 md:pt-0 md:h-[665px] grid grid-cols-1 md:grid-cols-2 items-center justify-between md:gap-4 p-4">
             <div className="md:ml-40">
-                <Card className="md:w-96">
-                    <CardHeader
-                        variant="gradient"
-                        color="gray"
-                        className="mb-4 grid h-28 place-items-center"
-                    >
-                        <Typography variant="h3" color="white">
-                            Registration
-                        </Typography>
-                    </CardHeader>
-                    <CardBody className="flex flex-col gap-4">
-                        <Input label="Email" size="lg" />
-                        <Input label="Password" size="lg" />
-                        <div className="-ml-2.5">
-                            <Checkbox label="Remember Me" />
-                        </div>
-                    </CardBody>
-                    <CardFooter className="pt-0">
-                        <Button variant="gradient" fullWidth>
-                            Register
-                        </Button>
-                        <Typography variant="small" className="mt-6 flex justify-center">
-                            Don&apos;t have an account?
-                           <Link to="/login">
-                           <Typography
-                            as="a"
-                            href=""
-                            variant="small"
-                            color="blue-gray"
-                            className="ml-1 font-bold"
+                <form onSubmit={handleRegister}>
+                    <Card className="md:w-96">
+                        <CardHeader
+                            variant="gradient"
+                            color="gray"
+                            className="mb-4 grid h-28 place-items-center"
                         >
-                            sign in
-                        </Typography>
-                           </Link>
-                        </Typography>
-                    </CardFooter>
-                </Card>
+                            <Typography variant="h3" color="white">
+                                Registration
+                            </Typography>
+                        </CardHeader>
+                        <CardBody className="flex flex-col gap-4">
+                            <Input name="name" label="Name" size="lg" />
+                            <Input name="email" label="Email" size="lg" />
+                            <Input name="password" label="Password" size="lg" />
+                            <div className="-ml-2.5">
+                                <Checkbox label="Remember Me" />
+                            </div>
+                        </CardBody>
+                        <CardFooter className="pt-0">
+                            <Button type="submit" variant="gradient" fullWidth>
+                                Register
+                            </Button>
+                            <Typography variant="small" className="mt-6 flex justify-center">
+                                Don&apos;t have an account?
+                                <Link to="/login">
+                                    <Typography
+                                        as="a"
+                                        href=""
+                                        variant="small"
+                                        color="blue-gray"
+                                        className="ml-1 font-bold"
+                                    >
+                                        sign in
+                                    </Typography>
+                                </Link>
+                            </Typography>
+                        </CardFooter>
+                    </Card>
+                </form>
             </div>
             <div>
-                <Lottie className="h-[500px]" animationData={RegisterAnimation}/>
+                <Lottie className="h-[500px]" animationData={RegisterAnimation} />
             </div>
         </div>
     );
