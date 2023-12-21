9016151b136aa394d7e8b3fec897413e499e229b
safimiazi <mohibullamiazi@gmail.com>
2023-12-21 14:55:25 +0600

    register page add

--- src/Components/Router/Router.jsx
+++ src/Components/Router/Router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../../Pages/Home";
 import Login from "../../Pages/Login";
+import Register from "../../Pages/Register";
 
 const router = createBrowserRouter([
     {
@@ -14,6 +15,10 @@ const router = createBrowserRouter([
         {
             path: "/login",
             element: <Login/>
+        },
+        {
+            path: "/register",
+            element: <Register/>
         }
       ]
     },
