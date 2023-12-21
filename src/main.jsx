574f1930c25f6e4417f6facf438dcf3ad915b294
safimiazi <mohibullamiazi@gmail.com>
2023-12-21 14:45:25 +0600

    router component create

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {
-  createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById('root')).render(
